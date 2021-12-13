1fd65e2387d5b4679e9535ec838f18c287b2ccfd
HBerglund <herman.e.berglund@gmail.com>
2021-12-13 12:34:42 +0100

    boilerplate setup: CRA + typescript + tailwindcss + classNames

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classNames from 'classnames';
+
+function App() {
+  return <div className={classNames('text-3xl')}>Hello world!</div>;
+}
+
+export default App;
