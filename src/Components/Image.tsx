341659dbad58db99b0b0ff0b755d91d3fb70a567
victorwikstrom <victor.wikstrom@icloud.com>
2021-12-15 14:52:16 +0100

    create image component

---
+++ src/Components/Image.tsx
@@ -0,0 +1,40 @@
+import classNames from 'classnames';
+import React from 'react';
+
+interface ImageProps {
+  imageUrl: string;
+  border?: true;
+  size: 'lg' | 'md' | 'sm';
+}
+
+const Image = ({ imageUrl, border, size }: ImageProps) => {
+  const getImageSize = () => {
+    switch (size) {
+      case 'lg':
+        return 'w-96 h-96';
+      case 'md':
+        return 'w-72 h-72';
+      case 'sm':
+        return 'w-56 h-56';
+    }
+  };
+
+  return (
+    <div
+      className={classNames(
+        border ? 'p-2 bg-gradient-to-l from-primary to-secondary' : '',
+        getImageSize(),
+        'rounded-full overflow-hidden relative'
+      )}
+    >
+      <img
+        className='w-full h-full object-cover rounded-full'
+        src={imageUrl}
+        alt=''
+      />
+      <div className='absolute inset-0 bg-gradient-to-r from-secondary to-tertiary opacity-10'></div>
+    </div>
+  );
+};
+
+export default Image;
