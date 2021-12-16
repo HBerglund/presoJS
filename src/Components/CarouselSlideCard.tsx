d6fb990d92805b38731b79396fe1a51b4bd0a2dd
puppets101 <oscand92@gmail.com>
2021-12-16 09:05:00 +0100

    Added image to carousel slide card

--- src/Components/CarouselSlideCard.tsx
+++ src/Components/CarouselSlideCard.tsx
@@ -1,27 +1,41 @@
 import React, { FC } from 'react';
 import classNames from 'classnames';
+import Image from './Image';
 
 type CarouselSlideProps = {
   content?: string;
   name?: string;
+  imageUrl?: string;
 };
 
 const CarouselSlideCard: FC<CarouselSlideProps> = ({
   content,
   name,
+  imageUrl,
 }: CarouselSlideProps) => {
   return (
     <div className='flex items-center bg-primary p-20 rounded-3xl border-2'>
       <div className={classNames('flex flex-col')}>
-        <span className={classNames('text-textPrimary text-body')}>
-          {content}
-        </span>
-        <span
-          className={classNames('text-textSecondary text-body text-left mt-4')}
-        >
-          {name}
-        </span>
+        {content && (
+          <span className={classNames('text-textPrimary text-body')}>
+            {content}
+          </span>
+        )}
+        {name && (
+          <span
+            className={classNames(
+              'text-textSecondary text-body text-left mt-4'
+            )}
+          >
+            {name}
+          </span>
+        )}
       </div>
+      {imageUrl && (
+        <div className={classNames('absolute top-2/3 left-1/4')}>
+          <Image border size='sm' imageUrl={imageUrl} />
+        </div>
+      )}
     </div>
   );
 };
--- src/data.tsx
+++ src/data.tsx
@@ -17,17 +17,21 @@ export const carouselCards: SlideType[] = [
   <CarouselSlideCard
     name='Oscar Andersson'
     content='"Being independent doesnt always translate to a better income. Both paths can enable you to make money, but being independent gives you way more creative freedom."'
+    imageUrl='https://media.istockphoto.com/photos/pleasant-young-indian-woman-freelancer-consult-client-via-video-call-picture-id1300972573'
   />,
   <CarouselSlideCard
     name='Herman Andersson'
     content='"Being independent doesnt always translate to a better income. Both paths can enable you to make money, but being independent gives you way more creative freedom."'
+    imageUrl='https://media.istockphoto.com/photos/middle-age-man-portrait-picture-id1285124274'
   />,
   <CarouselSlideCard
     name='Victor Andersson'
     content='"Being independent doesnt always translate to a better income. Both paths can enable you to make money, but being independent gives you way more creative freedom."'
+    imageUrl='https://media.istockphoto.com/photos/one-beautiful-woman-looking-at-the-camera-in-profile-picture-id1303539316'
   />,
   <CarouselSlideCard
     name='Bhaggy Andersson'
     content='"Being independent doesnt al creative freedom.3"'
+    imageUrl='https://media.istockphoto.com/photos/lord-vishwakarma-kaikadi-maharaj-math-pandharpur-picture-id1078413712'
   />,
 ];
